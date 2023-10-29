ed1c73c476612a846a7d771d5cf27c599aa360a4
erfan <erfanajoglu098@gmail.com>
2023-10-29 22:32:04 +0330

    Using the Fetch API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React , {useState , useEffect} from "react";
+
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading , setLoading] = useState(true);
+  const [tours , setTours] = useState([]);
+
+
+  const fetchData = async () =>{
+    setLoading(true);
+    try{
+      const response = await fetch(url) ;
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    }catch(error){
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  } , []);
   return (
-    <h1>Hello</h1>
-    );
+    <></>
+  );
 }
 
 export default App;
