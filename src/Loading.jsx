9de5a873f671b4d384379c9211655d719bcdbba3
erfan <erfanajoglu098@gmail.com>
2023-10-29 22:35:14 +0330

    Loading component created

---
+++ src/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+    return ( 
+        <div className="flex justify-center items-center">
+            <h1 className="font-bold">Loading...</h1>
+        </div>
+     );
+}
+ 
+export default Loading;
